5019f52874c915f3dc8d1078d79285bb7ff06842
Emman <91299895+emmanuel-joseph-22@users.noreply.github.com>
2023-05-18 17:50:54 +0800

    Update ReceiptWindow.java

--- ReceiptWindow.java
+++ ReceiptWindow.java
@@ -13,6 +13,26 @@ public class ReceiptWindow{
         Order menu = new Order();
         JFrame frame = new JFrame("Myprogram");
         
+        JPanel panel = new JPanel() {
+            protected void paintComponent(Graphics g) {
+                // Load the background image
+                Image backgroundImage = new ImageIcon("C:/Users/emman/OneDrive/Desktop/sample_2/design_img/background_receipt.png").getImage();
+                // Draw the background image
+                g.drawImage(backgroundImage, 0, 0, getWidth(), getHeight(), null);
+            }
+        };
+        // Set the layout manager to null for absolute positioning
+        panel.setLayout(null);
+        
+        // Set the size and position of the custom panel
+        panel.setPreferredSize(new Dimension(1550, 800));
+        panel.setBounds(0, 0, 1550, 800);
+        
+        JPanel CenterPanel = new JPanel();
+        CenterPanel.setBounds(450, 170, 650,480);
+        CenterPanel.setBackground(Color.WHITE);
+        CenterPanel.setLayout(null);
+        
         String[] col = {"QTY", "ITEM", "PRICE"};
         DefaultTableModel tableModel = new DefaultTableModel(col, 0);
         int size = menu.get_quantity_order().size();
@@ -36,51 +56,103 @@ public class ReceiptWindow{
         table.getColumnModel().getColumn(2).setPreferredWidth(20);
         table.getColumnModel().getColumn(2).setCellRenderer(rightRenderer);
         table.setRowHeight(25);
-        table.setBackground(Color.BLACK);
-        table.setForeground(Color.WHITE);
+        table.setBackground(Color.WHITE);
+        table.setForeground(Color.BLACK);
         table.setFont(new Font("Lucidas Sans Unicode", Font.PLAIN, 17));
-        table.getTableHeader().setBackground(Color.BLACK);
+        table.getTableHeader().setBackground(Color.WHITE);
         table.getTableHeader().setFont(new Font("Lucida Sans Unicode", Font.BOLD, 18));
-        table.getTableHeader().setForeground(Color.WHITE);
-        table.getTableHeader().setBorder(BorderFactory.createEtchedBorder());
-        
-        
-        
+        table.getTableHeader().setForeground(Color.BLACK);
+        table.getTableHeader().setBorder(BorderFactory.createEmptyBorder());
+        //table.getTableHeader().setBorder
         JScrollPane receipt = new JScrollPane(table);
+        receipt.getViewport().setBackground(Color.WHITE);
         receipt.setBorder(BorderFactory.createEmptyBorder());
-        receipt.getViewport().setBackground(Color.BLACK);
-        
+        receipt.setBounds(50, 20, 550, 300);
+        CenterPanel.add(receipt);
         
         int total = menu.get_total_of_total_price();
         JLabel total_amount = new JLabel("TOTAL: " + total + " php");
         total_amount.setFont(new Font("Lucida Sans Unicode", Font.BOLD, 22));
-        total_amount.setForeground(Color.WHITE);
-        
-        JPanel second_receipt = new JPanel(new FlowLayout(FlowLayout.RIGHT));
-        second_receipt.setPreferredSize(new Dimension(450, 50));
-        second_receipt.setBackground(Color.BLACK);
-        
-        
-                
+        total_amount.setBounds(400, 400, 200, 47);
+        total_amount.setForeground(Color.BLACK);
+        CenterPanel.add(total_amount);
+
         JLabel CustomerLabel = new JLabel();
         CustomerLabel.setText("CUSTOMER RECEIPT");
         CustomerLabel.setBounds(550, 20 , 500,100);
-        CustomerLabel.setFont(new Font("Verdana", Font.BOLD, 40));
+        CustomerLabel.setFont(new Font("Verdana", Font.PLAIN, 40));
         
         Color orange_button = new Color(255, 220, 175);
         
         JButton NextOrderButton = new JButton("NEXT CUSTOMER");
-        NextOrderButton.setFont(new Font("Lucida Sans Unicode", Font.BOLD, 15));
-        NextOrderButton.setBounds(720, 710, 200,55);
+        NextOrderButton.setFont(new Font("Verdana", Font.PLAIN, 15));
+        NextOrderButton.setBounds(770, 700, 200,55);
         //NextOrderButton.setEnabled(false);
         NextOrderButton.setBackground(orange_button);
         NextOrderButton.setBorder(BorderFactory.createEtchedBorder());
+        NextOrderButton.setFocusPainted(false);
         NextOrderButton.addActionListener(new ActionListener(){
             public void actionPerformed(ActionEvent e) {
                 frame.dispose();
                 new Myprogram();
             }
         });
+        
+        JButton ExitButton = new JButton("EXIT");
+        ExitButton.setBounds(600, 700, 120, 55);
+        ExitButton.setBackground(orange_button);
+        ExitButton.setFont(new Font("Verdana", Font.PLAIN, 15));
+        
+        ExitButton.setBorder(BorderFactory.createEtchedBorder());
+        ExitButton.setFocusPainted(false);
+        ExitButton.addActionListener(new ActionListener(){
+            public void actionPerformed(ActionEvent e) {
+                frame.dispose();
+            }
+        });
+
+        /*
+        JPanel RightPanel = new JPanel();
+        RightPanel.setPreferredSize(new Dimension(520,100));
+
+        JPanel LeftPanel = new JPanel();
+        LeftPanel.setPreferredSize(new Dimension(520,100));
+*/
+        Color color = new Color(95,196,203);
+        /*
+        JPanel UpperPanel = new JPanel();
+        UpperPanel.setPreferredSize(new Dimension(100,150 ));
+
+        JPanel LowerPanel = new JPanel();
+        LowerPanel.setPreferredSize(new Dimension(100,100));
+        */
+                // Create a custom JPanel for drawing the background image
+
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setExtendedState(JFrame.MAXIMIZED_BOTH);
+        frame.setSize(1550, 1000);
+        frame.setLayout(new BorderLayout());
+        frame.setVisible(true);
+        // Add other components to the custom panel or perform any desired customizations
+        // Set the custom panel as the content pane of the JFrame
+        frame.setContentPane(panel);
+        frame.add(CustomerLabel);
+        frame.add(NextOrderButton);
+        frame.add(ExitButton);
+        
+        //frame.add(void_order);
+        //frame.add(RightPanel, BorderLayout.EAST);
+        //frame.add(LeftPanel, BorderLayout.WEST);
+        //frame.add(U/pperPanel, BorderLayout.NORTH);
+        frame.add(CenterPanel);
+        //frame.add(LowerPanel, BorderLayout.SOUTH);
+        //CenterPanel.add(receipt);
+        //frame.add(second_receipt);
+        //second_receipt.add(total_amount);
+    }
+}
+
+ /*
         JButton void_order = new JButton("CANCEL ORDER");
         void_order.setBounds(950, 710, 170, 55);
         void_order.setBackground(orange_button);
@@ -94,56 +166,4 @@ public class ReceiptWindow{
                 frame.dispose();
                 new Myprogram();
             }
-        });
-        
-        JButton ExitButton = new JButton("EXIT");
-        ExitButton.setBounds(570, 710, 120, 55);
-        ExitButton.setBackground(orange_button);
-        ExitButton.setFont(new Font("Lucida Sans Unicode", Font.BOLD, 15));
-        ExitButton.setBorder(BorderFactory.createEtchedBorder());
-        ExitButton.addActionListener(new ActionListener(){
-            public void actionPerformed(ActionEvent e) {
-                frame.dispose();
-            }
-        });
-        JPanel CenterPanel = new JPanel();
-        CenterPanel.setPreferredSize(new Dimension(510,100));
-        CenterPanel.setBackground(Color.black);
-        
-        JPanel RightPanel = new JPanel();
-        RightPanel.setPreferredSize(new Dimension(520,100));
-        RightPanel.setBackground(Color.CYAN);
-
-        JPanel LeftPanel = new JPanel();
-        LeftPanel.setPreferredSize(new Dimension(520,100));
-        LeftPanel.setBackground(Color.CYAN);
-
-        Color color = new Color(95,196,203);
-
-        JPanel UpperPanel = new JPanel();
-        UpperPanel.setPreferredSize(new Dimension(100,150 ));
-        UpperPanel.setBackground(color);
-
-        JPanel LowerPanel = new JPanel();
-        LowerPanel.setPreferredSize(new Dimension(100,100));
-        LowerPanel.setBackground(color);
-
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setExtendedState(JFrame.MAXIMIZED_BOTH);
-        frame.setSize(1550, 1000);
-        frame.setLayout(new BorderLayout());
-        frame.setVisible(true);
-        frame.add(CustomerLabel);
-        frame.add(NextOrderButton);
-        frame.add(ExitButton);
-        frame.add(void_order);
-        frame.add(RightPanel, BorderLayout.EAST);
-        frame.add(LeftPanel, BorderLayout.WEST);
-        frame.add(UpperPanel, BorderLayout.NORTH);
-        frame.add(CenterPanel, BorderLayout.CENTER);
-        frame.add(LowerPanel, BorderLayout.SOUTH);
-        CenterPanel.add(receipt, BorderLayout.NORTH);
-        CenterPanel.add(second_receipt, BorderLayout.SOUTH);
-        second_receipt.add(total_amount);
-    }
-}
+        });*/
