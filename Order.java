7be8c1f8789954b5450e117a1086c09973c41fc7
Emman <91299895+emmanuel-joseph-22@users.noreply.github.com>
2023-05-20 18:35:49 +0800

    Update Order.java

--- Order.java
+++ Order.java
@@ -2,14 +2,16 @@ package Order;
 import java.util.ArrayList;
 
 public class Order {
+    //temporary storage for current customer order details
+    //used for displaying order receipt in receipt window
     private static ArrayList<Integer> quantity_order = new ArrayList<Integer>();
     private static int current_customer_no;
-    // for order eme
     private static ArrayList<String> food_order = new ArrayList<String>();
     private static ArrayList<Integer> total_price_order = new ArrayList<Integer>();
     private static ArrayList<Integer> food_id = new ArrayList<Integer>();
     private static ArrayList<Integer> stock_list = new ArrayList<>();
     
+    //getters and setters
     public ArrayList<Integer> get_quantity_order(){
             return quantity_order;
     }
@@ -34,11 +36,28 @@ public class Order {
     public int get_total_price_order(int i){
         return total_price_order.get(i);
     }
+
+    public static ArrayList<Integer> getStock_list() {
+        return stock_list;
+    }
+    
+    public static int getCurrent_customer_no() {
+        return current_customer_no;
+    }
+    
+    public static void setCurrent_customer_no(int current_customer_no) {
+        Order.current_customer_no = current_customer_no;
+    }
+    
+    //to clear data in arraylists 
+    //used once the customer order window starts
     public void clean(){
         food_order.clear();
         quantity_order.clear();
         total_price_order.clear(); 
     }
+    
+    //displaying total value in receipt 
     public int get_total_of_total_price(){
         int total = 0;
         for(int number : total_price_order){
@@ -46,21 +65,6 @@ public class Order {
         }
         return total;
     }
-
-    public static ArrayList<Integer> getStock_list() {
-        return stock_list;
-    }
-
-    public static void setStock_list(ArrayList<Integer> stock_list) {
-        Order.stock_list = stock_list;
-    }
     
-    
-    public static int getCurrent_customer_no() {
-        return current_customer_no;
-    }
 
-    public static void setCurrent_customer_no(int current_customer_no) {
-        Order.current_customer_no = current_customer_no;
-    }
 }
